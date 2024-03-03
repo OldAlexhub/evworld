ccf736ec8b524be762d33accae82eeafde38b000
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-03-02 23:51:15 -0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ import Signup from "../controllers/Signup.js";
 import Login from "../controllers/Login.js";
 import PostHealth from "../controllers/PostHealth.js";
 import ShowHealth from "../controllers/ShowHealth.js";
+import News from "../controllers/News.js";
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.post("/signup", Signup);
 router.post("/login", Login);
 router.post("/postdata", protectRoute, PostHealth);
 router.get("/show/:userId", protectRoute, ShowHealth);
+router.get("/news", protectRoute, News);
 
 export default router;
